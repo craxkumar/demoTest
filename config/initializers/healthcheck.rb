75adf000e4e776e9e88ded436d61a0b60aa3cd9f
Rakesh kumar <rakesh.c@comakeit.com>
2022-11-15 14:58:08 +0530

    updated workflow file

--- config/initializers/healthcheck.rb
+++ config/initializers/healthcheck.rb
@@ -19,3 +19,5 @@ Healthcheck.configure do |config|
   # config.add_check :cache,        -> { Rails.cache.read('some_key') }
   # config.add_check :environments, -> { Dotenv.require_keys('ENV_NAME', 'ANOTHER_ENV') }
 end
+
+
